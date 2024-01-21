6b17a623e48c095e8e9bce596f86d29f8410350b
antwanola <antwanola29@gmail.com>
2024-01-21 14:25:44 +0100

    The looping argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if num == 1:
         print("{} arguments.".format(num - 1))
     elif num == 2:
-        print("{} arguments".format(num - 1))
+        print("{} arguments:".format(num - 1))
     else:
-        print("{} argument".format(num - 1))
+        print("{} argument:".format(num - 1))
     for n in range(1, num):
         print("{}: {}".format((num -1), (sys.argv[n])))
