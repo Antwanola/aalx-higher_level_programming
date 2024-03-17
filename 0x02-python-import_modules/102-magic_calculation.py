91282c473a8a116136204e24ef2dc26f5c9f487c
antwanola <antwanola29@gmail.com>
2024-03-17 07:46:39 -0400

    The asci uppaercase printing in two lines

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-add, sub = __import__('magic_calculation_102', ('add', 'sub'))
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
