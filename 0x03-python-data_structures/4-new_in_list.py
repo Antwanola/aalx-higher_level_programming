03472301587ca2bbc645da92d7c108f456aa269f
antwanola <antwanola29@gmail.com>
2024-03-23 22:51:17 -0400

    print the copy of the modified list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,7 @@
 def new_in_list(my_list, idx, element):
     list_copy = my_list[:]
     if idx < 0 or idx >= my_list:
+        return (list_copy)
+    else:
         list_copy[idx] = element
         print("{}".format(list_copy))
